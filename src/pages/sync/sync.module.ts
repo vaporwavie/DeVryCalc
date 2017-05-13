c4ace5b7758d749725b643883218152f6dd735c5
Luiz Nickel <luizaugustonickel@gmail.com>
2017-05-13 11:56:52 -0300

    Remove dependencia não utilizada

--- src/pages/sync/sync.module.ts
+++ src/pages/sync/sync.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
-import { ComponentsModule } from '../../components/components.module';
 import { SyncPage } from './sync';
 
 @NgModule({
